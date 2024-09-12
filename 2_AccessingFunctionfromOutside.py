82441625172a6128903a6263e6a0086233f6e3c4
Samy-in <rojoldashec34@gmail.com>
2024-09-13 00:37:00 +0530

    Immutable & mutable and importing from different file

---
+++ 2_AccessingFunctionfromOutside.py
@@ -0,0 +1,15 @@
+#**** In python everything Becomes an object, So Immutable means the reference of a variable inside the
+#  memory will not change it will remain same .
+# Where Mutable means
+
+from Basic import Calculate
+
+Calculate(35)
+# here we also get to know about python shell by just writing python and it 
+# it opens an python environment where you can import the os , sys(system) where 
+# you can know your directory.
+
+
+# immutable and Mutable data type.
+# Immutable data type => list, set , dictionary, ByteArray, Array.
+# Mutable Data type => Integers , Floating-point numbers, String, Tuples,Boolean ,Frozen sets
---
+++ Basic.py
@@ -0,0 +1,7 @@
+print("This is the second time you are procastinating")
+
+def Calculate(sum):
+    print(2 * sum)
+    
+    
+Calculate(34)  
